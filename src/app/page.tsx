1614b220e71632b536b919e666508c9a68173784
Doikson <joseangelcortesbaillet@gmail.com>
2025-02-19 13:22:17 -0600

    Js template

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-white">
-      <h1 className="text-2xl font-bold text-gray-900">Jose Angel Cortes Baillet 🚀</h1>
-    </main>
-  );
-}
